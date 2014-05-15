f04badeb6227558c7655dfc6b11d3cb0bbc83396
parys <andrew_parys@mail.ru>
2014-05-15 14:29:09 +0300

    work user

--- Migrator/Program.cs
+++ Migrator/Program.cs
@@ -13,6 +13,7 @@ namespace Migrator
         {
             Program program = new Program();
             program.UpdateUsers();
+         //   program.UpdateBlogItems();
 
         }
 
@@ -249,8 +250,14 @@ namespace Migrator
                     i++;
                     // last modified
                     string lastDate = null;
+                    
                     while (chars[i] != '|')
                     {
+                        if (char.IsDigit(chars[i]) && chars[i + 1] == '\\')
+                        {
+                            i+=3;
+                        }
+                        
                         lastDate += chars[i];
                         i++;
                     }
@@ -267,7 +274,7 @@ namespace Migrator
 
         private void UpdateBlogItems()
         {
-            using (FileStream fs = new FileStream(@"C:\\.txt", FileMode.Open))
+            using (FileStream fs = new FileStream(@"C:\\blog.txt", FileMode.Open))
             {
                 byte[] data = new byte[fs.Length];
                 fs.Read(data, 0, Convert.ToInt32(fs.Length));
@@ -486,6 +493,236 @@ namespace Migrator
                     }
                  //   blogItem.LastModifiedUTC = long.Parse(lastDate);
                   //  UnitOfWork.BlogItemRepository.Add(blogItem);
+                    while (chars[i] != 10)
+                    {
+                        i++;
+                    }
+                }
+                //UnitOfWork.Save();
+            }
+        }
+
+        private void UpdateNewsItems()
+        {
+            using (FileStream fs = new FileStream(@"C:\\news.txt", FileMode.Open))
+            {
+                byte[] data = new byte[fs.Length];
+                fs.Read(data, 0, Convert.ToInt32(fs.Length));
+
+                char[] chars = Encoding.UTF8.GetString(data).ToCharArray();
+                for (int i = 0; i < chars.Length; i++)
+                {
+                    BlogItem blogItem = new BlogItem();
+                    // id
+                    string id = null;
+                    while (chars[i] != '|')
+                    {
+                        id += chars[i];
+                        i++;
+                    }
+                    blogItem.Id = int.Parse(id);
+                    // Category id
+                    string categoryId = null;
+                    while (chars[i] != '|')
+                    {
+                        categoryId += chars[i];
+                        i++;
+                    }
+                    blogItem.CategoryId = int.Parse(categoryId);
+                    // year
+                    string year = null;
+                    while (chars[i] != '|')
+                    {
+                        year += chars[i];
+                        i++;
+                    }
+                    blogItem.Year = int.Parse(year);
+                    // month
+                    string month = null;
+                    while (chars[i] != '|')
+                    {
+                        month += chars[i];
+                        i++;
+                    }
+                    blogItem.Month = int.Parse(month);
+                    // day
+                    string day = null;
+                    while (chars[i] != '|')
+                    {
+                        day += chars[i];
+                        i++;
+                    }
+                    blogItem.Day = int.Parse(day);
+                    // pending
+                    while (chars[i] != '|')
+                    {
+                        if (chars[i] == '1')
+                            blogItem.Pending = true;
+                        i++;
+                    }
+                    // onTop
+                    while (chars[i] != '|')
+                    {
+                        if (chars[i] == '1')
+                            blogItem.OnTop = true;
+                        i++;
+                    }
+                    //commentary_may
+                    while (chars[i] != '|')
+                    {
+                        if (chars[i] == '1')
+                            blogItem.CanCommentary = true;
+                        i++;
+                    }
+                    //add time
+                    string addTime = null;
+                    while (chars[i] != '|')
+                    {
+                        addTime += chars[i];
+                        i++;
+                    }
+                    blogItem.AdditionTime = long.Parse(addTime);
+                    // commentary Number
+                    string numberCommentary = null;
+                    while (chars[i] != '|')
+                    {
+                        numberCommentary += chars[i];
+                        i++;
+                    }
+                    blogItem.NumberCommentaries = int.Parse(numberCommentary);
+                    //author
+                    string userName = null;
+                    while (chars[i] != '|')
+                    {
+                        userName += chars[i];
+                        i++;
+                    }
+                    blogItem.User = UnitOfWork.UserRepository.Get(u => u.Login == userName).FirstOrDefault();
+                    //title
+                    string title = null;
+                    while (chars[i] != '|')
+                    {
+                        title += chars[i];
+                        i++;
+                    }
+                    blogItem.Title = title;
+                    //brief
+                    string brief = null;
+                    while (chars[i] != '|')
+                    {
+                        brief += chars[i];
+                        i++;
+                    }
+                    blogItem.Brief = brief;
+                    // message
+                    string message = null;
+                    while (chars[i] != '|')
+                    {
+                        message += chars[i];
+                        i++;
+                    }
+                    blogItem.Message = message;
+                    // attach
+                    while (chars[i] != '|')
+                    {
+                        id += chars[i];
+                        i++;
+                    }
+                    // files
+                    while (chars[i] != '|')
+                    {
+                        id += chars[i];
+                        i++;
+                    }
+                    // reads
+                    string reads = null;
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    blogItem.Reads = int.Parse(reads);
+                    // rating
+                    string rating = null;
+                    while (chars[i] != '|')
+                    {
+                        rating += chars[i];
+                        i++;
+                    }
+                    blogItem.Rating = byte.Parse(rating);
+                    // rate_num
+                    string ratingNumbers = null;
+                    while (chars[i] != '|')
+                    {
+                        ratingNumbers += chars[i];
+                        i++;
+                    }
+                    blogItem.RatingNumbers = int.Parse(ratingNumbers);
+                    // rate sum
+                    string ratingSumm = null;
+                    while (chars[i] != '|')
+                    {
+                        ratingSumm += chars[i];
+                        i++;
+                    }
+                    blogItem.RatingSumm = int.Parse(ratingSumm);
+                    //rate ip
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // other 1
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // other 2
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // other 3
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // other 4
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // other 5
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // last modified
+                    string lastDate = null;
+                    while (chars[i] != '|')
+                    {
+                        lastDate += chars[i];
+                        i++;
+                    }
+                    // uid
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    // subscr
+                    while (chars[i] != '|')
+                    {
+                        reads += chars[i];
+                        i++;
+                    }
+                    //   blogItem.LastModifiedUTC = long.Parse(lastDate);
+                    //  UnitOfWork.BlogItemRepository.Add(blogItem);
                     while (chars[i] != 10)
                     {
                         i++;
